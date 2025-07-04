60850379ed94cbc62a9fc8b7fb3a3eceaf747fdb
Jaeson-gram <freeonjeyson@gmail.com>
2025-07-04 12:45:09 +0100

    add migration

---
+++ Migrations/20250702212617_AddDepartmentsTable.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPI2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeptId",
+                table: "Students",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FacultyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "Id", "DepartmentName", "Description", "FacultyName" },
+                values: new object[,]
+                {
+                    { 1, "Systems Engineering, Design and Analysis", "systems from the ground up", "Computing" },
+                    { 2, "Artificial Intelligence", "making computers intelligent", "Computing" },
+                    { 3, "Cloud Computing", "computing on the cloud", "Computing" },
+                    { 4, "Quantum Physics", "the physics of small things", "Physical Sciences" },
+                    { 5, "Theoretical Physics", "when physics is simply observation and poetry", "Physical Sciences" },
+                    { 6, "Abstract Mathematics", "the maths of the unseen", "Mathematical Sciences" },
+                    { 7, "Applied Mathematics", "the maths of everyday things", "Mathematical Sciences" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateOfBirth", "DeptId" },
+                values: new object[] { new DateTime(2000, 4, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateOfBirth", "DeptId" },
+                values: new object[] { new DateTime(2006, 4, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateOfBirth", "DeptId" },
+                values: new object[] { new DateTime(2001, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_DeptId",
+                table: "Students",
+                column: "DeptId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Dept",
+                table: "Students",
+                column: "DeptId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Dept",
+                table: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_DeptId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "DeptId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "Students",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfBirth",
+                value: new DateOnly(2000, 4, 13));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateOfBirth",
+                value: new DateOnly(2006, 4, 7));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateOfBirth",
+                value: new DateOnly(2001, 3, 28));
+        }
+    }
+}
